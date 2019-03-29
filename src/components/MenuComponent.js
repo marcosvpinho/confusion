19ef38dc0b35055470712c3e371aa3481a2f02a1
Marcos Pinho <marcos.v.pinho@gmail.com>
2019-03-29 18:37:20 -0300

    React Router

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -10,7 +10,7 @@ import DishDetail from './DishdetailComponent';
                 <CardTitle>{dish.name}</CardTitle>
             </CardImgOverlay>
           </Card>
-
+        
         );
     }
     
