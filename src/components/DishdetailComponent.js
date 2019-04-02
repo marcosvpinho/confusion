c769c2420591f81ad0fcb83e97727ceb75855141
Marcos Pinho <marcos.v.pinho@gmail.com>
2019-04-01 23:30:14 -0300

    Single Pages App part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-	CardTitle, Media } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Media, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {Link} from 'react-router-dom';
 	
 	function RenderDish({dish}) {
-		return (
-			<Card>
-				<CardImg top src={dish.image} alt={dish.name} />
-				<CardBody>
-					<CardTitle>{dish.name}</CardTitle>
-					<CardText>{dish.description}</CardText>
-				</CardBody>
-			</Card>
-		);
+		if(dish){
+			return (
+				
+				<Card>
+					<CardImg top src={dish.image} alt={dish.name} />
+					<CardBody>
+						<CardTitle>{dish.name}</CardTitle>
+						<CardText>{dish.description}</CardText>
+					</CardBody>
+				</Card>
+				
+			);
+			}else{
+				return(
+					<div></div>
+				);
+			}
 	}
 	
 	function RenderComments({comments}) {
@@ -47,14 +55,24 @@ import { Card, CardImg, CardText, CardBody,
 		if (props.dish != null) 
 			return (
 				<div className="container">
-				<div className="row">
-					<div className="col-12 col-md-5 m-2">
-						<RenderDish dish={(props.dish)} />	
+					<div className="row">	
+						<Breadcrumb>
+							<BreadcrumbItem><Link to='/menu'>Menu</Link> </BreadcrumbItem>
+							<BreadcrumbItem active>{props.dish.name}}</BreadcrumbItem>
+						</Breadcrumb>
+						<div className="col-12">
+							<h3>{props.dish.name}</h3>
+							<hr/>
+                    	</div>
+                	</div>
+					<div className="row">
+						<div className="col-12 col-md-5 m-2">
+							<RenderDish dish={props.dish} />	
+						</div>
+						<div className="col-12 col-md-5 m-1">
+							<RenderComments comments= {props.comments}/>
+						</div>
 					</div>
-					<div className="col-12 col-md-5 m-1">
-						<RenderComments comments= {(props.dish.comments)}/>
-					</div>
-				</div>
 				</div>
 			);
 		else
