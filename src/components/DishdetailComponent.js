ca4a11ffb15a152f6ce1e215f7e21d307da21b65
Marcos Pinho <marcos.v.pinho@gmail.com>
2019-04-05 15:34:40 -0300

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,7 +2,8 @@ import React,  {Component} from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Media, Breadcrumb, BreadcrumbItem, Modal, Button, ModalHeader, ModalBody, Label, Row, Col } from 'reactstrap';
 import {Link} from 'react-router-dom';
 import {Control, LocalForm, Errors} from 'react-redux-form';
-	
+import {Loading} from './LoadingComponent';
+
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
@@ -54,7 +55,23 @@ const minLength = (len) => (val) => (val) && (val.length >= len);
 	
 	const DishDetail = (props) => {
 		console.log('DishDetail Component render invoked!');
-		if (props.dish != null) 
+		if(props.isLoading) {
+			return(
+				<div className="container">
+					<div className="row">
+						<Loading/>
+					</div>
+				</div>
+			);
+		}else if(props.errMess){
+			return(
+				<div className="container">
+					<div className="row">
+						<h4>{props.errMess}</h4>
+					</div>
+				</div>
+			);
+		}else if (props.dish != null) 
 			return (
 				<div className="container">
 					<div className="row">	
